43574c9538e941fb5d9fde61e091c0837d13c39b
fnedarb <braden.fink49@gmail.com>
2021-09-24 02:50:26 -0400

    began on piece update functionality

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,31 @@
 var myGamePiece;
-// let $playArea = $(playArea).get(0)
+timer = 0;
+lastTime = 0;
+bestTime = 0;
 
 function getRandomInt(piecePos) {
     return Math.floor(Math.random() * piecePos);
 }
+function storeBestTime() {
+    if(lastTime> 0 && lastTime<bestTime){
+        bestTime = lastTime
+    }
+}
+function timerDecrease(i) {
+    setTimeout(() => {timer--, 1000});
+    };
+
+function timerStart() {
+    for(let i=60; i>0; i--){
+    timerDecrease(i);
+    };
+};
+
+function timerRestart() {
+    timer = 60;
+};
+    
+console.log(timer)  
 
 let playArea = {
     canvas : document.createElement("canvas"),
@@ -12,20 +34,25 @@ let playArea = {
       this.canvas.height = 562.5;
       this.context = this.canvas.getContext("2d");
       document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-      }
-}
+      myGamePiece = new component(15, 15, "blue", getRandomInt(985), getRandomInt(547.5));
 
+}
+}
 function component(width, height, color, x, y) {
     this.width = width;
     this.height = height;
     this.x = x;
     this.y = y;
-    ctx = playArea.context;
-    ctx.fillStyle = color;
-    ctx.fillRect(this.x, this.y, this.width, this.height);
+    this.update = function(){
+        ctx = playArea.context;
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
 }
 function startGame() {
+    timerRestart();
     playArea.start();
-    myGamePiece = new component(15, 15, "blue", getRandomInt(1000), getRandomInt(562.5));
+    myGamePiece.update();
 }
+console.log(timer)  
 startGame();
