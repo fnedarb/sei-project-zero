84d4c66e3fb79acce2e6b03aae277481a2e6af44
fnedarb <braden.fink49@gmail.com>
2021-09-24 06:12:54 -0400

    refactored to use Path2D

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,9 @@ timer = 0;
 lastTime = 0;
 bestTime = 0;
 avgTime = 0;
+canvas = document.getElementById('gameCanvas');
+ctx = canvas.getContext('2d');
+
 function aimTrainer() {
     function getRandomInt(piecePos) {
         return Math.floor(Math.random() * piecePos);
@@ -31,71 +34,28 @@ function aimTrainer() {
     let playArea = {
         canvas : document.getElementById("gameCanvas"),
         start : function() {
-        //   this.canvas.width = 1000;
-        //   this.canvas.height = 562.5;
-        this.context = this.canvas.getContext("2d");
-        //   document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-        //   this.interval = setInterval(updatePlayArea, 20);
-        //   window.addEventListener('mousedown', function (e) {
-        //     playArea.x = e.pageX;
-        //     playArea.y = e.pageY;
-        //   })
-        //   window.addEventListener('mouseup', function (e) {
-        //     playArea.x = false;
-        //     playArea.y = false;
-        //   })
-        
+        this.context = this.canvas.getContext("2d");        
     },
         clear : function() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
-
     }
-
-    function component(width, height, color, x, y) {
-        this.width = width;
-        this.height = height;
-        this.x = x;
-        this.y = y;
-        this.build = function(){
-            ctx = playArea.context;
-            ctx.fillStyle = color;
-            ctx.fillRect(this.x, this.y, this.width, this.height);
-        }
-        // this.on("click"), function() {
-        //     updatePlayArea();
-        // }
-        // this.clicked = function() {
-        //     let myleft = this.x;
-        //     let myright = this.x + (this.width);
-        //     let mytop = this.y;
-        //     let mybottom = this.y + (this.height);
-        //     let clicked = true;
-        //     if ((mybottom < playArea.y) || (mytop > playArea.y) || (myright < playArea.x) || (myleft > playArea.x)) {
-        //       clicked = false;
-        //     }
-        //     return clicked;
-        // }
-
+    function buildTarget(){
+        let myGamePiece = new Path2D();
+        myGamePiece.arc(getRandomInt(985), getRandomInt(547.5), 20, 0, 2 * Math.PI);
+        ctx.fillStyle = 'red';
+        ctx.fill(myGamePiece);
     }
-    function updatePlayArea() {
-        playArea.clear();    
-        myGamePiece = new component(20, 20, "blue", getRandomInt(985), getRandomInt(547.5));
-        myGamePiece.build();
-    }
-    
+     function updatePlayArea() {
+        playArea.clear();   
+        buildTarget();
+    }    
     function startGame() {
         timerRestart();
         playArea.start();
         updatePlayArea();
         console.log(timer)
     }
-    // async function pieceClickUpdate(){
-    //     if (myGamePiece.clicked = true) {
-    //         updatePlayArea();          
-    //         }
-    // }
-    // playArea.onclick = updatePlayArea();
 startGame();
 }        
 aimTrainer();
