a0082626801065488a5cf5b85f5be8a53bd4ff6c
fnedarb <braden.fink49@gmail.com>
2021-09-24 03:22:51 -0400

    Added event listeners for target and play area update

--- scripts/app.js
+++ scripts/app.js
@@ -34,8 +34,15 @@ let playArea = {
       this.canvas.height = 562.5;
       this.context = this.canvas.getContext("2d");
       document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-      myGamePiece = new component(15, 15, "blue", getRandomInt(985), getRandomInt(547.5));
-    //   this.interval = setInterval(updateGameArea, 20);
+      this.interval = setInterval(updatePlayArea, 20);
+      window.addEventListener('mousedown', function (e) {
+        playArea.x = e.pageX;
+        playArea.y = e.pageY;
+      })
+      window.addEventListener('mouseup', function (e) {
+        playArea.x = false;
+        playArea.y = false;
+      })
   },
   clear : function() {
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -53,6 +60,17 @@ function component(width, height, color, x, y) {
         ctx.fillStyle = color;
         ctx.fillRect(this.x, this.y, this.width, this.height);
     }
+    this.clicked = function() {
+        let myleft = this.x;
+        let myright = this.x + (this.width);
+        let mytop = this.y;
+        let mybottom = this.y + (this.height);
+        let clicked = true;
+        if ((mybottom < playArea.y) || (mytop > playArea.y) || (myright < playArea.x) || (myleft > playArea.x)) {
+          clicked = false;
+        }
+        return clicked;
+    }
 }
 function updatePlayArea() {
     playArea.clear();
@@ -61,7 +79,9 @@ function updatePlayArea() {
 function startGame() {
     timerRestart();
     playArea.start();
+    myGamePiece = new component(15, 15, "blue", getRandomInt(985), getRandomInt(547.5));
     myGamePiece.update();
 }
+// myGamePiece.onclick(updatePlayArea());
 console.log(timer)  
 startGame();
